d93b89f95e5304eeeff748ac6105363423af78a7
Artur_M <64428009+ArturMykhalyk@users.noreply.github.com>
2025-06-05 11:52:54 +0300

    start

---
+++ src/js/modal.js
@@ -0,0 +1,28 @@
+//Описана робота модалки - відкриття закриття і все що з модалкою повʼязано
+import { refs } from './refs';
+export function openModal() {
+  refs.modal.classList.add('modal--is-open');
+
+  document.addEventListener('keydown', closeModalOnEsc);
+
+  window.addEventListener('click', clickNoModal);
+
+  refs.closeModalBtn.addEventListener('click', closeModal);
+}
+
+function closeModal() {
+  refs.modal.classList.remove('modal--is-open');
+  document.removeEventListener('keydown', closeModalOnEsc);
+  window.removeEventListener('click', clickNoModal);
+}
+
+function closeModalOnEsc(event) {
+  if (event.key === 'Escape') {
+    closeModal();
+  }
+}
+function clickNoModal(event) {
+  if (event.target === refs.modal) {
+    closeModal();
+  }
+}
---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+//Обʼєкт з посиланнями на ДОМ елементи
+
+export const refs = {
+  modal: document.querySelector('.modal'),
+  closeModalBtn: document.querySelector('.modal__close-btn'),
+};
