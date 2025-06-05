e49e58820f398aef7685291a993e553c7bc1a7b1
ArturMykhalyk <64428009+ArturMykhalyk@users.noreply.github.com>
2025-06-05 19:22:30 +0000

    Deploying to gh-pages from @ ArturMykhalyk/project-js-solo@a52bd4a3b4279e4c83ec11c3e0dc30dde5f72912 🚀

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-(function(){const n=document.createElement("link").relList;if(n&&n.supports&&n.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const i of o.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&s(i)}).observe(document,{childList:!0,subtree:!0});function d(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerPolicy&&(o.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?o.credentials="include":e.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function s(e){if(e.ep)return;e.ep=!0;const o=d(e);fetch(e.href,o)}})();const t={mobileMenuOpen:document.getElementById("open-menu"),mobileMenuClose:document.getElementById("close-btn"),mobileMenuList:document.querySelector(".header-menu"),modal:document.querySelector(".modal"),closeModalBtn:document.querySelector(".modal__close-btn")};function u(){t.mobileMenuClose.classList.remove("hidden"),t.mobileMenuOpen.classList.add("hidden"),t.mobileMenuList.classList.add("open"),t.mobileMenuClose.addEventListener("click",r),t.mobileMenuList.addEventListener("click",c),document.body.style.overflow="hidden"}function c(l){l.target.nodeName==="A"&&r()}function r(){document.body.style.overflow="",t.mobileMenuClose.classList.add("hidden"),t.mobileMenuOpen.classList.remove("hidden"),t.mobileMenuList.classList.remove("open"),t.mobileMenuClose.removeEventListener("click",r),t.mobileMenuList.removeEventListener("click",c)}t.mobileMenuOpen.addEventListener("click",u);
+import{a as d}from"./assets/vendor-C19taMLP.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const r of t.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&i(r)}).observe(document,{childList:!0,subtree:!0});function l(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=l(e);fetch(e.href,t)}})();async function m(n=1){try{const i="https://sound-wave.b.goit.study/api"+"/artists",e={limit:8,page:n};return(await d.get(i,{params:e})).data}catch(o){return console.log(o),null}}const s={mobileMenuOpen:document.getElementById("open-menu"),mobileMenuClose:document.getElementById("close-btn"),mobileMenuList:document.querySelector(".header-menu"),artistsList:document.querySelector(".artists-list"),modal:document.querySelector(".modal"),closeModalBtn:document.querySelector(".modal__close-btn")};function f(){s.mobileMenuClose.classList.remove("hidden"),s.mobileMenuOpen.classList.add("hidden"),s.mobileMenuList.classList.add("open"),s.mobileMenuClose.addEventListener("click",c),s.mobileMenuList.addEventListener("click",a),document.body.style.overflow="hidden"}function a(n){n.target.nodeName==="A"&&c()}function c(){document.body.style.overflow="",s.mobileMenuClose.classList.add("hidden"),s.mobileMenuOpen.classList.remove("hidden"),s.mobileMenuList.classList.remove("open"),s.mobileMenuClose.removeEventListener("click",c),s.mobileMenuList.removeEventListener("click",a)}function p(n){const o=n.map(({id:l,strArtist:i,strArtistThumb:e,strBiographyEN:t,genres:r})=>`<li class="artists--list__item">
+        <img
+          class="artists--photo"
+          src="${e}"
+          alt="${i}"
+        />
+        <ul class="artists--genres">
+        ${r.map(u=>`<li><p>${u}</p></li>`).join("")}
+        </ul>
+        <h3 class="artists--name">${i}</h3>
+        <p class="artists--descr">${t}</p>
+        <button class="artist--btn">
+          Learn More
+          <svg class="hero-icon" data-id="${l}">
+            <use href="/img/sprite.svg#icon-right-arrow"></use>
+          </svg>
+        </button>
+      </li>
+  `).join("");s.artistsList.innerHTML=o}s.mobileMenuOpen.addEventListener("click",f);async function b(){const n=await m();p(n.artists)}b();
 //# sourceMappingURL=index.js.map
