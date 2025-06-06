7fb5274fcaf15fef811530f6cb4c048afc8eae75
Artur_M <64428009+ArturMykhalyk@users.noreply.github.com>
2025-06-06 16:19:51 +0300

    about-us

--- src/home.js
+++ src/home.js
@@ -1,14 +1,30 @@
 import { getArtists } from './js/artists-api';
+import { getTotalPage, setTotalPage } from './js/constants';
+import { handleMoreBtn } from './js/handlers';
 import { handleOpenMobileMenu } from './js/mobile-menu';
 import { openModal } from './js/modal';
 import refs from './js/refs';
-import { createArtists } from './js/render-function';
+import {
+  createArtists,
+  hideLoader,
+  showLoader,
+  showLoadMoreButton,
+} from './js/render-function';
 // openModal();
 
 refs.mobileMenuOpen.addEventListener('click', handleOpenMobileMenu);
 async function initMainPage() {
+  showLoader();
   const artists = await getArtists();
+  hideLoader();
   createArtists(artists.artists);
+
+  setTotalPage(artists.totalArtists / 8);
+
+  if (getTotalPage() > 1) {
+    showLoadMoreButton();
+    refs.moreBtn.addEventListener('click', handleMoreBtn);
+  }
 }
 
 initMainPage();
---
+++ src/js/constants.js
@@ -0,0 +1,9 @@
+let totalPage = 0;
+
+export function setTotalPage(value) {
+  totalPage = value;
+}
+
+export function getTotalPage() {
+  return totalPage;
+}
