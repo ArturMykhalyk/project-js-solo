95c84072381ad7ebb4a931f139386cc402cba518
Artur_M <64428009+ArturMykhalyk@users.noreply.github.com>
2025-06-07 18:45:26 +0300

    айайацйца

--- src/js/render-function.js
+++ src/js/render-function.js
@@ -80,42 +80,42 @@ export function createFeedBack(feedBacks) {
       <div class="star-container">
         <div class="star">
           <svg class="star-empty">
-            <use href="/img/svg-icon/star-empty.svg"></use>
+            <use href="/img/sprite.svg#icon-star-empty"></use>
           </svg>
           <svg class="star-filled">
-            <use href="/img/svg-icon/star-full.svg"></use>
+            <use href="/img/sprite.svg#icon-star-full"></use>
           </svg>
         </div>
         <div class="star">
           <svg class="star-empty">
-            <use href="/img/svg-icon/star-empty.svg"></use>
+            <use href="/img/sprite.svg#icon-star-empty"></use>
           </svg>
           <svg class="star-filled">
-            <use href="/img/svg-icon/star-full.svg"></use>
+            <use href="/img/sprite.svg#icon-star-full"></use>
           </svg>
         </div>
         <div class="star">
           <svg class="star-empty">
-            <use href="/img/svg-icon/star-empty.svg"></use>
+            <use href="/img/sprite.svg#icon-star-empty"></use>
           </svg>
           <svg class="star-filled">
-            <use href="/img/svg-icon/star-full.svg"></use>
+            <use href="/img/sprite.svg#icon-star-full"></use>
           </svg>
         </div>
         <div class="star">
           <svg class="star-empty">
-            <use href="/img/svg-icon/star-empty.svg"></use>
+            <use href="/img/sprite.svg#icon-star-empty"></use>
           </svg>
           <svg class="star-filled">
-            <use href="/img/svg-icon/star-full.svg"></use>
+            <use href="/img/sprite.svg#icon-star-full"></use>
           </svg>
         </div>
         <div class="star">
           <svg class="star-empty">
-            <use href="/img/svg-icon/star-empty.svg"></use>
+            <use href="/img/sprite.svg#icon-star-empty"></use>
           </svg>
           <svg class="star-filled">
-            <use href="/img/svg-icon/star-full.svg"></use>
+            <use href="/img/sprite.svg#icon-star-full"></use>
           </svg>
         </div>
       </div>
