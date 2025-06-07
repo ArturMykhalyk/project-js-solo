4ec10ade498b7e26acfbecab3437a0860acf44fe
Artur_M <64428009+ArturMykhalyk@users.noreply.github.com>
2025-06-07 17:10:19 +0300

    cas

--- src/js/render-function.js
+++ src/js/render-function.js
@@ -80,10 +80,18 @@ export function createFeedBack(feedBacks) {
       <div class="star-container">
         <div class="star">
           <svg class="star-empty">
-            <use href="/img/sprite.svg#icon-star-empty"></use>
+            <use href="./img/sprite.svg#icon-star-empty"></use>
           </svg>
           <svg class="star-filled">
-            <use href="/img/sprite.svg#icon-star-full"></use>
+            <use href="./img/sprite.svg#icon-star-full"></use>
+          </svg>
+        </div>
+        <div class="star">
+          <svg class="star-empty">
+            <use href="./img/sprite.svg#icon-star-empty"></use>
+          </svg>
+          <svg class="star-filled">
+            <use href="./img/sprite.svg#icon-star-full"></use>
           </svg>
         </div>
         <div class="star">
@@ -104,18 +112,10 @@ export function createFeedBack(feedBacks) {
         </div>
         <div class="star">
           <svg class="star-empty">
-            <use href="/img/sprite.svg#icon-star-empty"></use>
+            <use href="img/sprite.svg#icon-star-empty"></use>
           </svg>
           <svg class="star-filled">
-            <use href="/img/sprite.svg#icon-star-full"></use>
-          </svg>
-        </div>
-        <div class="star">
-          <svg class="star-empty">
-            <use href="/img/sprite.svg#icon-star-empty"></use>
-          </svg>
-          <svg class="star-filled">
-            <use href="/img/sprite.svg#icon-star-full"></use>
+            <use href="img/sprite.svg#icon-star-full"></use>
           </svg>
         </div>
       </div>
